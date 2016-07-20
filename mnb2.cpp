4b2c99cab084420da95696c55cf7b793cf5d421a
aki55ram <aki55ram@gmail.com>
2016-07-20 18:26:56 +0530

    Add files via upload

---
+++ mnb2.cpp
@@ -0,0 +1,11 @@
+#include<iostream.h>
+void main()
+{
+char ch;
+cin>>a;
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+cout<<"Vowel";
+else
+cout<<"Consonant";
+}
+
