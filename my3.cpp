dda06a49c3c48043cf08f6cdbf609fc45d80a854
aki55ram <aki55ram@gmail.com>
2016-08-09 13:45:57 +0530

    Add files via upload

---
+++ my3.cpp
@@ -0,0 +1,48 @@
+#include <iostream.h>
+int main(void) {
+int n,i,j=0,a[100],a1[100];
+cin>>a;
+for(i=0;i<n;i++)
+cin>>a[i];
+for(i=0;i<n;i++)
+{
+	int c=0;
+	while(a[i])
+	{
+		int rem=a[i]%2;
+		a[i]=a[i]/2;
+		c++;
+    }
+    a1[j++]=c;
+}
+for(i=0;i<n;i++)
+{
+	for(j=0;j<n;j++)
+	{
+		if(a1[i]>a1[j])
+		{
+			int t=a1[i];
+			a1[i]=a1[j];
+			a1[j]=t;
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+		}
+		else if(a1[i]==a1[j])
+		{
+			if(a[i]>a[j])
+		{
+			int t=a1[i];
+			a1[i]=a1[j];
+			a1[j]=t;
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+		}	
+		}
+	}
+}
+for(i=0;i<n;i++)
+cout<<a[i];
+return 0;
+}
